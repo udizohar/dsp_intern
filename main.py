e0760287211ebf958bd3bd39d18e50bc1d7820d9
udizohar <97758861+udizohar@users.noreply.github.com>
2026-01-29 20:17:46 +0200

    read 2 frames

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import cv2
 import os
+import numpy as np
 
 def load_video(input_dir, video_name, output_dir):
     video_path = os.path.join(input_dir, video_name)
@@ -8,6 +9,11 @@ def load_video(input_dir, video_name, output_dir):
     fps = cap.get(cv2.CAP_PROP_FPS)
     frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
     duration_sec = frame_count / fps if fps > 0 else None
+
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+    print("Resolution:", width, "x", height)
     print("fps = ", fps)
 
     frame_idx = 0
@@ -24,9 +30,32 @@ def load_video(input_dir, video_name, output_dir):
     print(f"Saved {frame_idx} frames")
 
 
+def get_motion_two_images(K, img_first, img_second):
+    return 100, 200
+
 if __name__ == '__main__':
     input_dir = "video_input"
     video_name = "1.mp4"
     output_dir = "frames"
 
-    load_video(input_dir, video_name, output_dir)
+    #load_video(input_dir, video_name, output_dir)
+    image_width = 1920
+    image_height = 1080
+    focal_xy = 1620
+
+    K = np.array([focal_xy, 0, image_width / 2,
+                    0, focal_xy, image_height / 2,
+                    0, 0, 1], dtype=np.float64)
+
+    recording_folder_name = "1"
+    folder_path = os.path.join(output_dir, recording_folder_name)
+    first_frame_idx = 0
+    second_frame_idx = 1
+    first_image_path = os.path.join(folder_path, f"frame_{first_frame_idx:06d}.png")
+    second_image_path = os.path.join(folder_path, f"frame_{second_frame_idx:06d}.png")
+    img_first = cv2.imread(first_image_path)
+    img_second = cv2.imread(second_image_path)
+
+    x, y = get_motion_two_images(K, img_first, img_second)
+
+    print(x,y)
